ef91a091c113b4b2dfd22a31b541d4bdfb006f8f
pro11oficial <contatopro11.oficial@gmail.com>
2026-04-07 22:59:06 -0300

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const admin = require("firebase-admin");
 const app = express();
 app.use(express.json());
 
-const serviceAccount = require("./firebase-key.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
